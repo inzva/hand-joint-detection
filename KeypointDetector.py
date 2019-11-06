53ce6939cd92a9674817a1c11a2f623a18d19cfc
ahmet <ahmetmeleq@gmail.com>
2019-11-06 22:15:11 +0300

    Added 2 modes of movement

--- KeypointDetector.py
+++ KeypointDetector.py
@@ -8,93 +8,18 @@ from model import CPM_Model
 from config import FLAGS
 from keras.models import load_model
 from keras.models import model_from_json
-
-
-
-
-
-
-
-#____________________________________________________________________________________________________________________________________________________________________________________________________________________________
-from tkinter import *
-from tkinter import scrolledtext
-from action_interface import *
+import pyautogui as mouse
 import time
 
-
-window = Tk()
-
-window.title("Tele Mouse")
-window.geometry('350x275')
+mouse.FAILSAFE = False
 
 
-#lbl = Label(window, text="Click to start!", font = ('Comic Sans MS',25))
-#lbl.grid(column=0, row=0)
+limiter =1
+counter = 0
+control_distance = 55
+mov_speed = 0.25
+last_click_time = time.time()
 
-
-def clicked():
-
-    btn.configure(text="Button was clicked !!")
-
-
-btn = Button(window, text="Click Me", command = clicked)
-
-btn.grid(column=0, row=5)
-
-def leftKey(event):
-    myActor.test_action('10000000')
-    return
-
-def rightKey(event):
-    myActor.test_action('01000000')
-    return
-
-def upKey(event):
-    myActor.test_action('00100000')
-    return
-
-def downKey(event):
-    myActor.test_action('00010000')
-    return
-
-myActor = Actor()
-
-frame = Frame(window, width=100, height=100)
-window.bind('<Left>', leftKey)
-window.bind('<Right>', rightKey)
-window.bind('<Up>', upKey)
-window.bind('<Down>', downKey)
-frame.grid()
-
-txt = scrolledtext.ScrolledText(window,width=40,height=10)
-txt.grid(column=0,row=0)
-
-txt.config(state=DISABLED)
-
-
-#____________________________________________________________________________________________________________________________________________________________________________________________________________________________
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#ahm = load_model('my_model.h5')
 
 
 graph1 = tf.Graph()
@@ -105,12 +30,24 @@ with graph1.as_default():
 			ahm = model_from_json(arch_file.read())
 		ahm.load_weights('model.h5')
 
+
+
+
 joint_detections = np.zeros(shape=(21, 2))
 
-#ahm = model_from_json(loaded_model_json)
-#ahm.load_weights("model.h5")
 
-global myvar3
+
+
+
+
+
+
+
+
+
+
+
+
 def main(argv):
 
 
@@ -131,6 +68,10 @@ def main(argv):
 	device_count = {'GPU': 1} if FLAGS.use_gpu else {'GPU': 0}
 	sess_config = tf.ConfigProto(device_count=device_count)
 
+
+
+
+
 	with tf.Session(config=sess_config) as sess:
 
 		saver.restore(sess, 'model/cpm_hand')
@@ -146,42 +87,11 @@ def main(argv):
 			joint_kalman_filter.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]],
 														   np.float32) * FLAGS.kalman_noise
 
+
+
 		while True:
 
-			loopfunc(ahm,model,output_node,cam,kalman_filter_array,tracker,sess,joint_detections)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def loopfunc(ahm,model,output_node,cam,kalman_filter_array,tracker,sess,joint_detections):
 			_, full_img = cam.read()
 
 			test_img = tracker.tracking_by_joints(full_img, joint_detections=joint_detections)
@@ -196,50 +106,17 @@ def loopfunc(ahm,model,output_node,cam,kalman_filter_array,tracker,sess,joint_de
 										feed_dict={model.input_images: test_img_input})
 
 			print('FPS: %.2f' % (1 / (time.time() - t1)))
-			#for item in joint_detections:
-			#	print(item)
+
+
+
 
 			local_img = visualize_result(full_img, stage_heatmap_np, kalman_filter_array, tracker, crop_full_scale,
 										 test_img_copy)
 
 
-			j2 = np.reshape(joint_detections,(1,42))
-			j2= np.append(j2,0)
-			j2= np.append(j2,0)
-			j2 = np.reshape(j2,(1,44))
 
-
-			try:
-				if(j2[0,19]>430):
-					myActor.test_action('10000000')
-				elif(j2[0,19]<120):
-					myActor.test_action('01000000')
-				if(j2[0,18]>380):
-					myActor.test_action('00010000')
-				if(j2[0,18]<120):
-					myActor.test_action('00100000')
-			except:
-				print('not passed yet')
-
-			#print(j2.shape)
-
-			with graph1.as_default():
-				
-				with session1.as_default():
-					print(j2)
-					a=ahm.predict(j2)
-			print(a)
-			myvar3=str(list(((a.max(axis=1,keepdims=1) == a) + [0,0,0,0,0,0,0,0])[0]))
-			print(np.shape(myvar3),type(myvar3))
-			print(myvar3)
-			myActor.act(myvar3)
 			cv2.imshow('global_img', full_img.astype(np.uint8))
 			if cv2.waitKey(1) == ord('q'): print("To abort: Ctrl + C")
-			window.update_idletasks()
-			window.update()
-
-
-
 
 
 
@@ -261,6 +138,10 @@ def normalize_and_centralize_img(img):
 	return test_img_input
 
 
+
+
+
+
 def visualize_result(test_img, stage_heatmap_np, kalman_filter_array, tracker, crop_full_scale, crop_img):
 	demo_stage_heatmaps = []
 
@@ -275,12 +156,17 @@ def visualize_result(test_img, stage_heatmap_np, kalman_filter_array, tracker, c
 	return crop_img
 
 
+
+
+
+
 def correct_and_draw_hand(full_img, stage_heatmap_np, kalman_filter_array, tracker, crop_full_scale, crop_img):
 	global joint_detections
 	joint_coord_set = np.zeros((FLAGS.num_of_joints, 2))
 	local_joint_coord_set = np.zeros((FLAGS.num_of_joints, 2))
 
 	mean_response_val = 0.0
+
 
 
 	for joint_num in range(FLAGS.num_of_joints):
@@ -302,11 +188,10 @@ def correct_and_draw_hand(full_img, stage_heatmap_np, kalman_filter_array, track
 		correct_coord[1] += tracker.bbox[2]
 		joint_coord_set[joint_num, :] = correct_coord
 
-	#for item in joint_coord_set:
-	#	print(item)
+
 
 	draw_hand(full_img, joint_coord_set, tracker.loss_track)
-	draw_hand(crop_img, local_joint_coord_set, tracker.loss_track)
+	#draw_hand(crop_img, local_joint_coord_set, tracker.loss_track)
 	joint_detections = joint_coord_set
 
 	if mean_response_val >= 1:
@@ -318,48 +203,141 @@ def correct_and_draw_hand(full_img, stage_heatmap_np, kalman_filter_array, track
 				org=(20, 20), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(255, 0, 0))
 
 
+
+
+
+
+
+
+
+
 def draw_hand(full_img, joint_coords, is_loss_track):
+
+	global control_distance	
+	global mov_speed
+
+
+
+
 	if is_loss_track:
 		joint_coords = FLAGS.default_hand
 
 	
 	for joint_num in range(FLAGS.num_of_joints):
-		#color_code_num = (joint_num // 4)
-		#radiN = 5
-		#jointS = 255 - (joint_num%5)*50
-		cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=4, color=(255,255,255), thickness=-1)	
-		'''
-		if joint_num in [0]:
-			joint_color = (jointS,0,0)
-			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=7,
-					   color=joint_color, thickness=-1)	
-		elif joint_num in [0,1,2,3,4]:
-			joint_color = (jointS,0,0)
-			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=radiN,
-					   color=joint_color, thickness=-1)		
-		elif joint_num in [5,6,7,8,9]:
-			joint_color = (0,jointS,0)
-			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=radiN,
-					   color=joint_color, thickness=-1)
-		elif joint_num in [10,11,12,13,14]:
-			joint_color = (0,0,jointS)
-			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=radiN,
-					   color=joint_color, thickness=-1)
+
+
+		if(joint_num!=9):
+			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=4, color=(255,255,255), thickness=-1)
+
+	
 		else:
-			joint_color = (jointS,jointS,jointS)
-			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=radiN,
-					   color=joint_color, thickness=-1)
-		'''	
-	'''
-		elif joint_num in [4, 8, 12, 16]:
-			#joint_color = list(map(lambda x: x + 35 * (joint_num % 4), FLAGS.joint_color_code[color_code_num]))
-			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=3,
-					   color=joint_color, thickness=-1)
-		else:
-			#joint_color = list(map(lambda x: x + 35 * (joint_num % 4), FLAGS.joint_color_code[color_code_num]))
-			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=3,
-					   color=joint_color, thickness=-1)
-	'''
+			#We get inside this else only for our control joint.
+	
+			#reference point
+			cv2.circle(full_img, center=(328, 199), radius=5, color=(0,255,0), thickness=-1)
+
+			#controlling point
+			cv2.circle(full_img, center=(int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0])), radius=4, color=(0,0,255), thickness=-1)	
+
+			#reference circle
+			cv2.circle(full_img, center=(328, 199), radius=control_distance, color=(0,255,0), thickness=3)	
+			#print('COORD',int(joint_coords[joint_num][0]),int(joint_coords[joint_num][1]))
+
+
+
+			rect_hor_len = 200	
+			rect_ver_len = 100
+
+			rect_left = int(328 - (rect_hor_len/2))
+			rect_right = rect_left + rect_hor_len
+
+
+			rect_up = 199 + control_distance + 50
+			rect_down = rect_up + rect_ver_len
+			
+			#print(rect_up,rect_down,rect_left,rect_right)
+			
+		
+
+			cv2.line(full_img, (rect_left,rect_up), (rect_right,rect_up), color = (0,0,255) ) 
+			cv2.line(full_img, (rect_left,rect_down), (rect_right,rect_down), color = (0,0,255) )
+			cv2.line(full_img, (rect_left,rect_down), (rect_left,rect_up), color = (0,0,255) )
+			cv2.line(full_img, (rect_right,rect_down), (rect_right,rect_up), color = (0,0,255) )
+
+
+
+			distance = math.sqrt((joint_coords[joint_num][0]-199)**2 + (joint_coords[joint_num][1]-328)**2)
+			g_dist_x = joint_coords[4][1] - joint_coords[20][1]
+			print('GDIST',g_dist_x)
+
+			global last_click_time
+			time_passed = time.time() - last_click_time
+			if(g_dist_x<0 and time_passed>1.5):
+				mouse.click()
+				last_click_time = time.time()
+
+			if(distance > control_distance and g_dist_x>100):
+				#We get in here only if we are outside the reference circle
+
+				if(joint_coords[joint_num][0]<rect_up):
+					#print('RECT UP VS CONT POINT',joint_coords[joint_num][1],rect_up)
+					#print('Dot is OUTSIDE. Distance: {0}'.format(distance))
+
+
+
+					global counter, limiter
+
+					if(counter==limiter):
+						counter = 0
+
+						mydif = (joint_coords[joint_num][1]-328,joint_coords[joint_num][0]-199)
+						mymov = ( mydif[0] * mov_speed , mydif[1] * mov_speed)
+
+						mouse.moveRel(mymov[0],mymov[1], duration=0.05)
+
+
+					else:
+						counter = counter + 1
+				
+				else:
+					control_point = (int(joint_coords[joint_num][1]), int(joint_coords[joint_num][0]))
+					loc_ratio = ((control_point[0]-rect_left)/rect_hor_len , (control_point[1]-rect_up)/rect_ver_len)
+					myloc = (int(loc_ratio[0]*1920),int(loc_ratio[1]*1080))
+					print('Second Mode Active. MYLOC: ',myloc[0],myloc[1])
+					g_dist_x = joint_coords[0][0] - joint_coords[20][0]
+					print('GDISTX',g_dist_x)
+					if(joint_coords[joint_num][1]<rect_down and joint_coords[joint_num][0]>rect_left and joint_coords[joint_num][0]<rect_right and g_dist_x>100 ):
+						mouse.moveTo(myloc[0],myloc[1])
+
+					else:
+						continue
+
+
+			else:
+				#print('Dot is INSIDE. Distance: {0}'.format(distance))
+				continue
+
+
+
+	draw_limbs(full_img,joint_coords)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def draw_limbs(full_img,joint_coords):
 
 	for limb_num in range(len(FLAGS.limbs)):
 		x1 = int(joint_coords[int(FLAGS.limbs[limb_num][0])][0])
@@ -376,6 +354,8 @@ def draw_hand(full_img, joint_coords, is_loss_track):
 			color_code_num = limb_num // 4
 			limb_color = list(map(lambda x: x + 35 * (limb_num % 4), FLAGS.joint_color_code[color_code_num]))
 			cv2.fillConvexPoly(full_img, polygon, color=limb_color)
+
+
 
 
 if __name__ == '__main__':
